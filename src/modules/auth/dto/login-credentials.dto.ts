66d4abe40c74270163fe160a7a32fb6b4dff0891
Dima 🙃 <dmitry.chernitsov@huk-coburg.de>
2020-12-17 22:38:43 +0100

    fix login credentials dto

--- src/modules/auth/dto/login-credentials.dto.ts
+++ src/modules/auth/dto/login-credentials.dto.ts
@@ -1,27 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsString,
-  MinLength,
-  MaxLength,
-  Matches,
-  IsDefined,
-} from 'class-validator';
+import { IsString, IsDefined } from 'class-validator';
 
 export class LoginCredentialsDto {
   @ApiProperty()
   @IsDefined()
   @IsString()
-  @MinLength(4)
-  @MaxLength(20)
   readonly username: string;
 
   @ApiProperty()
   @IsDefined()
   @IsString()
-  @MinLength(8)
-  @MaxLength(20)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'password too weak',
-  })
   readonly password: string;
 }
